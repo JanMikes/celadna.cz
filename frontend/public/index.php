db505d73a3a972cfb7bced7f3cb84751f3ff6e90
Jan Mikeš <j.mikes@me.com>
2022-03-22 14:03:58 +0100

    Initial commit of frontend app

--- frontend/public/index.php
+++ frontend/public/index.php
@@ -1,3 +1,9 @@
-<?php
+<?php declare(strict_types=1);
 
-echo 'Hello world';
+use Celadna\Website\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
