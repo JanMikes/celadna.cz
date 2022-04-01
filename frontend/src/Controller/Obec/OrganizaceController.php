95e9152e22e1504eb5e4fa6bab823dc3330c530f
Jan Mikeš <j.mikes@me.com>
2022-04-01 15:41:39 +0200

    Obec - Organizace controller

--- frontend/src/Controller/Obec/OrganizaceController.php
+++ frontend/src/Controller/Obec/OrganizaceController.php
@@ -4,15 +4,24 @@ declare(strict_types=1);
 
 namespace Celadna\Website\Controller\Obec;
 
+use Celadna\Website\Content\Content;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class OrganizaceController extends AbstractController
 {
+    public function __construct(
+        private Content $contentProvider
+    ) {}
+
+
     #[Route('/obec/organizace', 'obec_organizace')]
     public function __invoke(): Response
     {
-        return $this->render('obec_organizace.html.twig');
+        return $this->render('obec_organizace.html.twig', [
+            'graficke_pasy' => $this->contentProvider->getOrganizaceData(),
+            'footer' => $this->contentProvider->getFooterData(),
+        ]);
     }
 }
