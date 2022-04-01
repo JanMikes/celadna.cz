8874748637f41f1727c6e956bdf8081972149feb
Jan Mikeš <j.mikes@me.com>
2022-04-02 00:28:39 +0200

    Mapa stranek controller

---
+++ frontend/src/Controller/MapaStranekController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Celadna\Website\Controller;
+
+use Celadna\Website\Content\Content;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class MapaStranekController extends AbstractController
+{
+    public function __construct(
+        private Content $contentProvider
+    ) {}
+
+
+    #[Route('/mapa-stranek', 'mapa_stranek')]
+    public function __invoke(): Response
+    {
+        return $this->render('mapa_stranek.html.twig', [
+            'footer' => $this->contentProvider->getFooterData(),
+        ]);
+    }
+}
