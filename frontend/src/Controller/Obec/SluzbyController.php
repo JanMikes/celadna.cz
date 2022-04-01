617810b5a6b4da75cc501d38ed48e0a8036bd323
Jan Mikeš <j.mikes@me.com>
2022-04-01 15:46:12 +0200

    Obec - Sluzby controller

--- frontend/src/Controller/Obec/SluzbyController.php
+++ frontend/src/Controller/Obec/SluzbyController.php
@@ -4,15 +4,24 @@ declare(strict_types=1);
 
 namespace Celadna\Website\Controller\Obec;
 
+use Celadna\Website\Content\Content;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class SluzbyController extends AbstractController
 {
+    public function __construct(
+        private Content $contentProvider
+    ) {}
+
+
     #[Route('/obec/sluzby', 'obec_sluzby')]
     public function __invoke(): Response
     {
-        return $this->render('obec_sluzby.html.twig');
+        return $this->render('obec_sluzby.html.twig', [
+            'sluzby' => $this->contentProvider->getSluzbyData(),
+            'footer' => $this->contentProvider->getFooterData(),
+        ]);
     }
 }
