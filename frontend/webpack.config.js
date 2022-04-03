5b22a8c7b654a049b518f99327f72f08498dbd17
Jan Mikeš <j.mikes@me.com>
2022-04-03 17:01:19 +0200

    Header

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -42,8 +42,7 @@ Encore
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+    .enableVersioning()
 
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-proposal-class-properties');
