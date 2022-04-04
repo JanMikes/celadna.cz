83669afa7ec5a410d7fdd15c074200f605e51227
Jan Mikeš <j.mikes@me.com>
2022-04-04 10:21:01 +0200

    Strapi admin - Obec

---
+++ strapi/src/api/obec/controllers/obec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  obec controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::obec.obec');
---
+++ strapi/src/api/obec/services/obec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * obec service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::obec.obec');
