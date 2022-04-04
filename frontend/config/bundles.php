399852248a70b62bee6e18b971b121e5fb1194c8
Jan Mikeš <j.mikes@me.com>
2022-04-05 00:46:10 +0200

    Sentry

--- frontend/config/bundles.php
+++ frontend/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     EAG\EasyHydrator\EasyHydratorBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
