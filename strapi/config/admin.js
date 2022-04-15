164946d7e81cce46279aef64248df96e3bb4ff38
Jan Mikeš <j.mikes@me.com>
2022-04-15 12:23:03 +0200

    Update strapi version to gain massive speedup

--- strapi/config/admin.js
+++ strapi/config/admin.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   url: '/dashboard',
   host: env('ADMIN_HOST', 'localhost'),
+  salt: env('API_TOKEN_SALT'),
   auth: {
     secret: env('ADMIN_JWT_SECRET', '6022cb72a2adab1b01d9da9314c82c31'),
   },
